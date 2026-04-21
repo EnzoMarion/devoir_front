9c4e8408ef41b3c45cffd2406796780495333908
EnzoMarion <marionenzo26@gmail.com>
2026-04-21 16:22:49 +0200

    v3 fin du tp3

--- devoir-front_exo3/src/App.jsx
+++ devoir-front_exo3/src/App.jsx
@@ -1,46 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import PWABadge from './PWABadge.jsx'
 import './App.css'
 
 function App() {
-    const [formData, setFormData] = useState({ title: '' })
     const [results, setResults] = useState([])
 
-    const handleSubmit = async (event) => {
-        event.preventDefault()
-
-        const query = encodeURIComponent(formData.title())
-        const url = `https://dev.to/api/articles?top=1&per_page=10`
+    useEffect(() => {
+        const fetchArticles = async () => {
+            const url = 'https://dev.to/api/articles?top=1&per_page=10'
 
         try {
             const response = await fetch(url)
             const data = await response.json()
-            setResults(data.features || [])
+            setResults(data)
         } catch (error) {
             console.error(error)
         }
     }
 
+        fetchArticles()
+    }, [])
+
     return (
         <>
             <div>
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label htmlFor="title">Title : </label>
-                        <input
-                            type="text"
-                            name="title"
-                            value={formData.title}
-                            onChange={(e) =>
-                                setFormData({ ...formData, title: e.target.value })
-                            }
-                        />
-                    </div>
-                    <button type="submit">Envoyer</button>
-                </form>
                 <div>
-                    {results.map((feature) => (
-                        <div key={feature.properties.id}>{feature.properties.label}</div>
+                    {results.map((article) => (
+                        <div key={article.id}>
+                            <a href={article.url} target="_blank" rel="noreferrer">
+                                {article.title}
+                            </a>
+                        </div>
                     ))}
                 </div>
             </div>
